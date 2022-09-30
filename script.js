0a29a668f71a147af45a2a7644abf23fb303b55c
Adrigondo <adrigondo.profesional@gmail.com>
2022-09-29 20:25:29 -0600

    Update: show end matrix

--- script.js
+++ script.js
@@ -21,9 +21,11 @@ function startCalculations() {
     return ecuationsMatrix;
   }
 
-  var ecuationMatrix = getInputsValues();
+  var ecuationsMatrix = getInputsValues();
 
-  var x = calculateXValues(ecuationMatrix, 3);
+  var x = calculateXValues(ecuationsMatrix, 3);
+
+  console.log(ecuationsMatrix);
   printResults(x);
 }
 function calculateXValues(ecuationsMatrix, n) {
@@ -41,9 +43,9 @@ function calculateXValues(ecuationsMatrix, n) {
       ecuationsMatrix[i][k] = 0;
       ecuationsMatrix[i][0] -= factor * ecuationsMatrix[k][0];
       // console.log(ecuationsMatrix[i]);
-      // console.log(ecuationsMatrix);
     }
   }
+  printMatrix('end', ecuationsMatrix);
   x[n] = ecuationsMatrix[n][0] / ecuationsMatrix[n][n];
 
   for (let i = n - 1; i >= 1; i--) {
@@ -64,6 +66,75 @@ function calculateXValues(ecuationsMatrix, n) {
 function round(number, decimalsQuantity) {
   let temp = 10 ** decimalsQuantity;
   return Math.round((number + Number.EPSILON) * temp) / temp;
+}
+function printMatrix(identifier, matrix) {
+  var matrixTable = document.getElementById(`${identifier}-matrix-table-js`);
+  var matrixContent = document.getElementById(`${identifier}-matrix-js`);
+  matrixTable.classList.remove('not-visible');
+  matrixContent.innerHTML = null;
+
+  var newRow;
+  var newCeld;
+
+  newRow = document.createElement('tr');
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[1][1];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[1][2];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[1][3];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[1][0];
+  newRow.appendChild(newCeld);
+
+  matrixContent.appendChild(newRow);
+
+  newRow = document.createElement('tr');
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[2][1];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[2][2];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[2][3];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[2][0];
+  newRow.appendChild(newCeld);
+
+  matrixContent.appendChild(newRow);
+
+  newRow = document.createElement('tr');
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[3][1];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[3][2];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[3][3];
+  newRow.appendChild(newCeld);
+
+  newCeld = document.createElement('td');
+  newCeld.innerHTML = matrix[3][0];
+  newRow.appendChild(newCeld);
+
+  matrixContent.appendChild(newRow);
 }
 
 function printResults(x) {
